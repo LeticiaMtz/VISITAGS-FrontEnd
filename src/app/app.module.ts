d83e549a4949c650db7711cabfb36076229ba56a
miguel.salazar <miguel.salazar@utags.edu.mx>
2020-10-14 15:10:20 -0500

    Hotfix de interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,17 +15,9 @@ import { RegisterComponent } from './login/register.component';
 import { ServiceModule } from './services/service.module';
 import { AuthGuard } from './guards/auth.guard';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptorService } from './services/login/token-interceptor.service';
+import { AuthInterceptorService } from './services/login/token-interceptor.service';
 import { ChangePasswordComponent } from './restore-password/change-password/change-password.component';
 import { SendEmailComponent } from './restore-password/send-email/send-email.component';
-import { ModalityComponent } from './pages/modality/modality.component';
-
-
-
-
-
-
-
 
 @NgModule({
   declarations: [
@@ -46,7 +38,7 @@ import { ModalityComponent } from './pages/modality/modality.component';
     FormsModule,
     FilePondModule
   ],
-  providers: [AuthGuard, {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true}],
+  providers: [AuthGuard, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/login/token-interceptor.service.ts
+++ src/app/services/login/token-interceptor.service.ts
@@ -1,22 +1,57 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
-import { LoginService } from './login.service';
-
-
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService implements HttpInterceptor {
+export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private loginService: LoginService) { }
+  constructor(
+    private router: Router
+  ) {}
 
-  intercept(req, next) {
-    const tokeized = req.clone({
-      setHeader: {
-        Authorization: `Bearer ${this.loginService.getToken()}`
-      }
-    });
-    return next.handle(tokeized);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string = localStorage.getItem('aa_token');
+
+    let request = req;
+
+    if (token) {
+      request = req.clone({
+        setHeaders: {
+          aa_token: token
+        }
+      });
+    }
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+
+        if (err.status === 401) {
+          Swal.fire(
+            'Opsss!',
+            'Lo sentimos su sesion ha caducado, por favor vuelva a iniciar sesion',
+            'error'
+          );
+          this.router.navigateByUrl('/login');
+        }
+
+        if (err.status === 403) {
+          Swal.fire(
+            'Opsss!',
+            'No cuentas con los permisos necesarios para acceder a este menú.',
+            'error'
+          );
+          this.router.navigateByUrl('/dashboard');
+        }
+
+        return throwError( err );
+
+      })
+    );
   }
+
 }
