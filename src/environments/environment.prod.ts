2b4be5a7d2eae641837dac8346386238b6b30d90
Palacios <luisma.ibpa@outlook.com>
2020-11-17 14:38:46 -0600

    hot fix - se cambió la ruta del proyecto de angular para producción, ademas, se corrigio un inconvenite con el monitor de alerta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   // urlGlobal: 'http://localhost:3000/api',
   //urlGlobal: 'http://172.16.50.41:3000/api',
-  urlGlobal: 'https://seguimiento-tutoral.fs.utags.edu.mx/api',
+  urlGlobal: 'https://seguimiento-tutoral.api.fs.utags.edu.mx/api',
+              
   production: true,
 
   nuevo:       '5f186c5de9475240bc59e4a7',
